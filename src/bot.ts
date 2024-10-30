eeb1d02d078698e92b9ad0067656e5ed4ac34593
ruslannazari <nazarmuhamedowruslan@gmail.com>
2024-10-30 18:01:26 +0500

    Fixed issue

--- src/bot.ts
+++ src/bot.ts
@@ -7,10 +7,10 @@ const {
 // Set your token in the vercel environment variable
 export const bot = new Bot(token)
 
-bot.command("start", async (ctx:Context)=>{
+bot.command("start", (ctx:Context)=>{
     ctx.reply("Привет, это бот на TS)")
 })
 // attach all middleware
-bot.on('message', async ctx => {
-    await ctx.reply(ctx.message.text!)
+bot.on('message', ctx => {
+    ctx.reply(ctx.message.text!)
 })
