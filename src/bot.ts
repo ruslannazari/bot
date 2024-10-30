3f742155c199bd0015586c342012587ae7f5721c
ruslannazari <nazarmuhamedowruslan@gmail.com>
2024-10-30 17:13:18 +0500

    Init commit

---
+++ src/bot.ts
@@ -0,0 +1,16 @@
+import { Bot, Context } from 'grammy'
+
+const {
+    BOT_TOKEN: token = '7714266406:AAESvtQV2DpsC1No6rzgS9WcFg8MQHdBcjM',
+} = process.env
+
+// Set your token in the vercel environment variable
+export const bot = new Bot(token)
+
+bot.command("start", async (ctx:Context)=>{
+    ctx.reply("Привет, это бот на TS)")
+})
+// attach all middleware
+bot.on('message', async ctx => {
+    await ctx.reply(ctx.message.text!)
+})
