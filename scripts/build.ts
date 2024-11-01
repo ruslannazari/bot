3e176dcc86c6b25a8b6977f2182804b0dc44e1c3
ruslannazari <nazarmuhamedowruslan@gmail.com>
2024-11-01 11:17:41 +0500

    Added logging in build.ts

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,5 +1,8 @@
 import { bot } from '../src/bot'
 
+bot.init()
+console.info('Info:', bot.botInfo)
+
 const {
     VERCEL_URL: host,
     // set your webhook address or use default Vercel deployment url
@@ -12,3 +15,6 @@ console.log(webhook)
 console.log(process.env.BOT_TOKEN)
 
 bot.api.setWebhook(webhook)
+const data = bot.api.getWebhookInfo()
+
+console.log('Webhook set to URL:',data)
