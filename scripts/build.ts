68d2430331ac81dceebbd2f23319afba248a275a
ruslannazari <nazarmuhamedowruslan@gmail.com>
2024-11-01 12:05:04 +0500

    Fixed issue

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,19 +1,19 @@
 import { bot } from '../src/bot'
 
 bot.init()
-
+console.info('Info:', bot.botInfo)
 
 const {
-    VERCEL_URL: host,
-    // set your webhook address or use default Vercel deployment url
-    WEBHOOK: webhook = `https://${host}/api/webhook`,
+    VERCEL_URL = 'localhost',
+    VERCEL_BRANCH_URL: hostname = VERCEL_URL,
 } = process.env
 
-bot.catch(console.error)
+const url = new URL('api/webhook', `https://${hostname}`)
 
-console.log(webhook)
+bot.catch(console.error)
+bot.api.setWebhook(url.href)
+
 console.log(process.env.BOT_TOKEN)
 
-bot.api.setWebhook(webhook)
 console.log("Done")
 console.log(bot.api.token)
